40a4f18e9aa0f2e2e267a09f1bae17ccf8328055
Itai Keren <itaik1993@gmail.com>
2023-11-12 13:33:17 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,11 @@
 import RandomPicker from "./components/random-picker";
+import { participants } from './participants';
 
 export default function App() {
-  const namesList = [
-    "Yuval Shoshan",
-    "Matan Tessler",
-    "Victor Isarov",
-    "Ron Ben-Haim",
-    "Itai Keren",
-    "Noam Baron",
-    "Eldad Tzadok",
-    "Guy Bercovich",
-    "Tal Bar-On",
-    "David Sellam",
-    "Jonathan Levi",
-    "Sapir Rahmani",
-    "Adi Moshe",
-    "Shira Attias",
-    "Adi Yaacobi",
-    "Sahar Nahaisi",
-    "Artyom Ivanov - Bug accured please Re-Roll",
-    "Lior Baraban",
-    "Omer Furman",
-    "Nir Elbaz",
-    "Yuval Glizerin",
-    "Laure Montuelle",
-    "Loren Goldstein",
-    "Ran Jarufi",
-    "Kim Belassen",
-    "Kim Jong Un",
-    "Donald Trump",
-    "Elon Musk",
-    "BeeBot"
-  ];
 
   return (
     <div className="flex flex-col h-screen w-screen m-auto">
-      <RandomPicker items={namesList} duration={1000} />
+      <RandomPicker items={participants} duration={1000} />
     </div>
   );
 }
